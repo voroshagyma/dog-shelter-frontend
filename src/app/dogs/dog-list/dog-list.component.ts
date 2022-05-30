c97cd7b6431c7a4a360fbf998d6e9b4574f19aa9
stakkk <jon.doe.dummystring@gmail.com>
2022-05-30 08:26:08 +0200

    Add generating dogs matrix and add bootstrap grid

--- src/app/dogs/dog-list/dog-list.component.ts
+++ src/app/dogs/dog-list/dog-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { IDog } from '../models/idog';
 
 @Component({
@@ -13,7 +13,31 @@ export class DogListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+
+    if (changes['dogs']) {
+
+      const dogs = changes['dogs'].currentValue as IDog[];
+
+      this.dogsToDogMatrix(dogs);
+    }
+
+  }
+
   @Input()
   dogs: IDog[] = [];
 
+  dogsMatrix: IDog[][] = [];
+
+  private dogsToDogMatrix(dogs: IDog[]) {
+    const matrix = [];
+    const dogsCopy = dogs.slice();
+    while (dogsCopy.length) {
+      matrix.push(dogsCopy.splice(0, 4));
+    }
+
+    this.dogsMatrix = matrix;
+
+    console.log(this.dogsMatrix);
+  }
 }
