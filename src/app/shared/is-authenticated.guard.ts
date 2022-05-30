4e58e04cd11aaaf039f4b0a0f039dfdeb242e733
proeszti <jon.doe.dummystring@gmail.com>
2022-05-30 20:33:17 +0200

    Implement simple auth guard

--- src/app/shared/is-authenticated.guard.ts
+++ src/app/shared/is-authenticated.guard.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UsersService } from '../users/users.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IsAuthenticatedGuard implements CanActivate {
 
+  constructor(private readonly usersService: UsersService, private readonly router: Router) { }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+    if (!!this.usersService.user) {
+      return true;
+    }
+    else {
+      return this.router.navigate(["/"]);
+    }
+
   }
 
 }
