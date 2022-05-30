d1ab83398be733400c8611427f365a6f40a1f1f3
stakkk <jon.doe.dummystring@gmail.com>
2022-05-30 09:22:45 +0200

    Update pager

--- src/app/dogs/dog-page/dog-page.component.ts
+++ src/app/dogs/dog-page/dog-page.component.ts
@@ -11,10 +11,34 @@ export class DogPageComponent implements OnInit {
 
   constructor(private readonly dogService: DogsService) { }
 
+  currentPage: number = 0;
+  pageSize = 20;
+
   dogs: IDog[] = [];
 
+  slicedDogs: IDog[] = [];
+
   ngOnInit(): void {
-    this.dogService.findAll().subscribe(dogs => this.dogs = dogs.slice(0, 20));
+    this.dogService.findAll().subscribe(dogs => {
+      this.dogs = dogs;
+      this.sliceDogs();
+    });
+  }
+
+  handleNext(value: number) {
+    this.currentPage = value;
+    this.sliceDogs();
+  }
+
+  handlePrevious(value: number) {
+    this.currentPage = value;
+    this.sliceDogs();
+  }
+
+  private sliceDogs() {
+    const startIndex = this.currentPage * this.pageSize + this.currentPage;
+    const endIndex = startIndex + this.pageSize;
+    this.slicedDogs = this.dogs.slice(startIndex, endIndex);
   }
 
 }
--- src/app/shared/pager/pager.component.ts
+++ src/app/shared/pager/pager.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-pager',
@@ -10,6 +10,42 @@ export class PagerComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  currentPage: number = 0;
+
+  @Input()
+  list: any[] = [];
+
+  @Input()
+  pageSize: number = 1;
+
+  @Output()
+  onPrevious = new EventEmitter<number>();
+
+  @Output()
+  onNext = new EventEmitter<number>();
+
+  handlePrevious() {
+
+    if (this.currentPage === 0) {
+      return;
+    }
+
+    this.currentPage = this.currentPage - 1;
+    this.onPrevious.emit(this.currentPage);
+  }
+
+  handleNext() {
+
+    const maxIndex = Math.ceil(this.list.length / this.pageSize);
+
+    if (this.currentPage === maxIndex) {
+      return;
+    }
+
+    this.currentPage = this.currentPage + 1;
+    this.onNext.emit(this.currentPage);
   }
 
 }
